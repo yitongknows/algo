f3299043515f08f6b64c30d15a6e7cf1b4ff59a4
yitongknows <61811966+yitongknows@users.noreply.github.com>
2021-09-10 01:29:50 -0400

    Update find-mountain-peak.py

--- src/find-mountain-peak.py
+++ src/find-mountain-peak.py
@@ -2,6 +2,27 @@ class Solution:
     """
     @param A: An integers array.
     @return: return any of peek positions.
+    There is an integer array which has the following features:
+
+The numbers in adjacent positions are different.
+A[0] < A[1] && A[A.length - 2] > A[A.length - 1].
+We define a position P is a peak if:
+
+A[P] > A[P-1] && A[P] > A[P+1]
+Find a peak element in this array. Return the index of the peak.
+
+It's guaranteed the array has at least one peak.
+The array may contain multiple peeks, find any of them.
+The array has at least 3 numbers in it.
+样例
+Example 1:
+
+Input:
+
+A = [1, 2, 1, 3, 4, 5, 7, 6]
+Output:
+
+1
     """
     def findPeak(self, A):
         # arr has at least 3 element, no need to check error
